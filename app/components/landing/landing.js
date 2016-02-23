f6984a707739bd44a0670b634a765b684bcc4529
Avery Duffin <averyduffin@gmail.com>
2016-02-23 13:31:21 -0600

    updating after adding some style details

--- app/components/landing/landing.js
+++ app/components/landing/landing.js
@@ -5,5 +5,9 @@
 var LandingPageControllers = angular.module('LandingPageControllers', []);
 
 LandingPageControllers.controller('landingPageController', ['$scope', function($scope){
-
+	$scope.displayForm = false;
+	
+	$scope.start = function(){
+		$scope.displayForm = true;
+	}
 }]);
