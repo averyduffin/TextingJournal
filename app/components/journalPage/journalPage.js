730c5cc09e18f80fec8c7af14282f3d3e1bcc0df
Avery Duffin <averyduffin@gmail.com>
2016-03-04 11:14:25 -0600

    fixed the front end to allow journal entries to display.

--- app/components/journalPage/journalPage.js
+++ app/components/journalPage/journalPage.js
@@ -8,15 +8,8 @@
 var page1Controllers = angular.module('page1Controllers', []);
 
 page1Controllers.controller('page1Controller', ['$scope', '$rootScope', '$location', 'AuthenticationService', '$routeParams', 'Entries', function($scope, $rootScope, $location, AuthenticationService, $routeParams, Entries){
-	console.log($rootScope.globals);
+	//console.log($rootScope.globals);
 	
-	/*$scope.user = new Users();
-	$scope.user.$get({id: $rootScope.globals.currentUser.id}, function(data){
-		console.log(data);
-	},
-	function(err){
-		console.error(err);
-	});*/
 	$scope.fullName = $rootScope.globals.currentUser.fullname;
 	if($rootScope.globals.currentUser.backgroundURL == null){
 		$scope.backgroundURL = "https://placeholdit.imgix.net/~text?txtsize=55&txt=1160%C3%97500&w=1160&h=500";
@@ -32,10 +25,15 @@ page1Controllers.controller('page1Controller', ['$scope', '$rootScope', '$locati
 	}
 	
 	$scope.entries = new Entries();
-	$scope.entries.beforeDate = new Date().toLocaleString();
-	$scope.entries.afterDate = new Date().toLocaleString(); //This still needs to be implimented
+	var currdate = new Date()
+	var pastdate = new Date();
+	pastdate.setDate(new Date().getDate()- 10)
+
+	$scope.entries.beforeDate = currdate.getFullYear() + currdate.getMonth() + currdate.getDate();
+	$scope.entries.afterDate = pastdate.getFullYear() + pastdate.getMonth() + pastdate.getDate();
 	$scope.entries.$get({id: $rootScope.globals.currentUser.id}, function(data){
 		console.log(data);
+		$scope.entries = data.entries;
 	},
 	function(err){
 		console.error(err);
